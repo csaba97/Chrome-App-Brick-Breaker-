01d62fd6b7ba628446e0c6238b60fb2ee463f543
csaba97 <csabagabor97@google.com>
2018-08-16 21:04:53 +0300

    basic ball handling

--- gameplay.js
+++ gameplay.js
@@ -19,6 +19,7 @@ var brick = {
   height : 20,
 }
 
+
 var colorsBrick = {
   1 : "blue",
   2 : "purple",
@@ -38,6 +39,14 @@ var paddle = {
   posY : canvas.height - 40,
 }
 
+var ball = {
+  diameter : 20,
+  posX : paddle.posX,
+  posY : paddle.posY-10,
+  direction : 45,
+  speed : 15
+}
+
 window.addEventListener("load", startGame);
 window.onload = function() {
   document.body.insertBefore(canvas, document.body.childNodes[0]);
@@ -49,7 +58,10 @@ window.onload = function() {
 
 function startGame() {
   writeMessage("Score: "+score);
+  drawBall();
+  drawPaddle();
   drawSceneBricks();
+  updateGameplay();
 }
 
 function initialiseCanvas(){
@@ -68,11 +80,22 @@ function writeMessage(message) {
         context.fillText(message, 10, 25);
 }
 
+function drawBall(){
+  context.fillStyle = "rgb(51,51,0)";
+  context.beginPath();
+  context.arc(ball.posX,ball.posY,ball.diameter/2,0,2*Math.PI);
+  context.fill();
+}
+
+function drawPaddle(){
+  context.fillStyle = "rgb(0,51,0)";
+  context.fillRect(paddle.posX-paddle.width/2, paddle.posY, paddle.width, paddle.height);
+}
+
 function movePaddle(x){
   context.clearRect(paddle.posX-paddle.width/2, paddle.posY, paddle.width, paddle.height);
   paddle.posX = x;
-  context.fillStyle = "rgb(0,51,0)";
-  context.fillRect(paddle.posX-paddle.width/2, paddle.posY, paddle.width, paddle.height);
+  drawPaddle();
 }
 
 function getMousePos(evt) {
@@ -89,7 +112,7 @@ function drawSceneBricks(){
   var startingPositionY = 40;
   for (var i=0, len=arr.length; i<len; i++) {
     for (var j=0, len2=arr[i].length; j<len2; j++) {
-        if(arr[i][j]!=0){
+        if(arr[i][j]!==0){
           context.fillStyle = colorsBrick[arr[i][j]];
           context.fillRect(startingPositionX+j*brick.width,startingPositionY+i*brick.height , brick.width, brick.height);
           context.rect(startingPositionX+j*brick.width,startingPositionY+i*brick.height , brick.width, brick.height);
@@ -97,4 +120,57 @@ function drawSceneBricks(){
         }
       }
     }
+}
+
+function updateGameplay(){
+  var fps=10;
+  var refresh=1000/fps;
+  setInterval(function(){ clearBall();moveBall();drawBall(); }, refresh);
+}
+
+function clearBall(){
+
+}
+
+function moveBall(){
+
+  var x = ball.posX, y = ball.posY;
+  var speed = ball.speed;
+  var direction = ball.direction;
+  console.log(direction);
+  switch(direction){
+    case 0:x+=speed; break;
+    case 45:x+=speed;y-=speed; break;
+    case 90:y-=speed; break;
+    case 135:x-=speed;y-=speed; break;
+    case 180:x-=speed; break;
+    case 225:x+=speed;y+=speed; break;
+    case 270:y+=speed; break;
+    case 315:x+=speed;y+=speed; break;
+  }
+
+  if(x>canvas.width){
+    x = canvas.width;
+    if(direction === 315) direction = 225;
+    if(direction === 45) direction = 135;
+  }
+  if(x<0){
+    x = 0;
+    if(direction === 225) direction = 315;
+    if(direction === 135) direction = 45;
+  }
+  if(y>canvas.height){
+    y = canvas.height;
+    if(direction === 315) direction = 45;
+    if(direction === 225) direction = 135;
+  }
+  if(y<0){
+    if(direction === 135) direction = 225;
+    if(direction === 45) direction = 315;
+    y=0;
+  }
+
+  ball.posX = x;
+  ball.posY = y;
+  ball.direction = direction;
 }
